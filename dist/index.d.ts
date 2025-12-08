51d3e0766c1eef0e65003db832ca50abe2415d01
Agu Chux <agu.chux@golojan.co.uk>
2025-12-08 05:22:34 +0000

    version 1.7 update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 export * from "./authenticator";
 export * from "./env";
-export { default as GolojanProvider } from "./provider";
+export * from "./providers/next-auth";
+export * from "./providers/oath";
 //# sourceMappingURL=index.d.ts.map
